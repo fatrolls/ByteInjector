aadef6479286d093eb207e64e7bc248e8890b3ca
fatrolls <fatrolls@users.noreply.github.com>
2021-07-07 00:25:13 -0700

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,27 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDD_DIALOG1                     100
+#define IDI_MAIN_ICON                   101
+#define IDI_ICON1                       104
+#define IDC_BTN_INJECT                  1000
+#define IDC_EDIT_PROCESS                1002
+#define IDC_BTN_UNLOAD                  1008
+#define IDC_BTN_REFRESH                 1009
+#define IDC_BTN_DLL                     1010
+#define IDC_EDIT_DLL                    1011
+#define IDC_LIST_PROCESSES              1012
+#define IDC_BTN_PROCESS                 1014
+#define IDC_CBX_AUTOINJECT              1015
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
